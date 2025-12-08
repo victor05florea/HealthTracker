01fd945ae93d8e0dad599e21ce933752863a8975
Florea Victor <victor05florea@gmail.com>
2025-12-08 03:41:42 +0200

    Conectare frontend cu backend: Hello World functional

--- backend/src/main/java/com/victor/healthtracker/HealthController.java
+++ backend/src/main/java/com/victor/healthtracker/HealthController.java
@@ -1,4 +1,15 @@
 package com.victor.healthtracker;
 
+import org.springframework.web.bind.annotation.CrossOrigin; // <-- Asigura-te ca ai importul asta
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*") // <-- Permite accesul oricui (telefonului)
 public class HealthController {
+
+    @GetMapping("/test")
+    public String sayHello() {
+        return "Salut! Backend-ul Java functioneaza!";
+    }
 }
