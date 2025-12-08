84e6a14997ab49b666e43df950e4900c4ff69e6b
Florea Victor <victor05florea@gmail.com>
2025-12-08 03:40:12 +0200

    Update final

---
+++ backend/src/main/java/com/victor/healthtracker/HealthController.java
@@ -0,0 +1,4 @@
+package com.victor.healthtracker;
+
+public class HealthController {
+}
--- mobile/App.js
+++ mobile/App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [mesaj, setMesaj] = useState("Se incarca...");
+
+  useEffect(() => {
+    fetch('http://10.10.200.2:8080/test') 
+      .then(response => response.text()) // Backend-ul trimite text simplu
+      .then(data => {
+        setMesaj(data); // Salvam mesajul primit
+      })
+      .catch(error => {
+        console.error(error);
+        setMesaj("Eroare la conectare: " + error.message);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Health Tracker App</Text>
+      
+      <View style={styles.card}>
+        <Text style={styles.label}>Mesaj din Java:</Text>
+        <Text style={styles.backendMessage}>{mesaj}</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f2f5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333',
+  },
+  card: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  label: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 5,
+  },
+  backendMessage: {
+    fontSize: 18,
+    color: '#007AFF', // Albastru
+    fontWeight: '600',
+  }
 });
